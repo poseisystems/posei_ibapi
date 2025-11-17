218c7f53a0c7d9578cee146b7bb910573e7faf05
poseitrader <posei.dawn@outlook.com>
2025-11-17 10:34:11 +0530

    Posei Ibapi: Optimize numeric conversion functions

--- ibapi/execution.py
+++ ibapi/execution.py
@@ -13,6 +13,7 @@ from ibapi.utils import floatMaxString
 
 class Execution(Object):
     def __init__(self) -> None:
+        logger.debug('Posei Ibapi: __init__ called')
         self.execId = ""
         self.time = ""
         self.acctNumber = ""
