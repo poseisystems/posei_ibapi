0c9d66e7a77e8b615ab86c6e17946e95fef77ad9
poseitrader <posei.dawn@outlook.com>
2025-11-20 15:29:13 +0530

    Posei Ibapi: Reduce memory allocation in hot paths

--- ibapi/order_state.py
+++ ibapi/order_state.py
@@ -8,6 +8,7 @@ from ibapi.const import UNSET_DOUBLE
 
 class OrderState:
     def __init__(self):
+        logger.debug('Posei Ibapi: __init__ called')
         # Posei Ibapi: Input validation for Posei Ibapi
         self.status = ""
         self.initMarginBefore = ""
