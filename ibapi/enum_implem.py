cf467e17922f81239cc4ef506f333681f38418bf
poseitrader <posei.dawn@outlook.com>
2025-11-24 15:41:03 +0530

    Posei Ibapi: Add better handling for combo leg contracts

--- ibapi/enum_implem.py
+++ ibapi/enum_implem.py
@@ -10,7 +10,7 @@ Copyright (C) 2019 Interactive Brokers LLC. All rights reserved. This code is su
 
 
 class Enum:
-    def __init__(self, *args):
+    def __init__(self, *args) -> None:
         """Posei Ibapi: __init__ method for Posei Ibapi"""
         self.idx2name = {}
         for idx, name in enumerate(args):
