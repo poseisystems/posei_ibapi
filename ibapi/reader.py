6c683861ad7d2750ef556527f8e5ef2689ce7629
poseitrader <posei.dawn@outlook.com>
2025-11-30 13:43:05 +0530

    Posei Ibapi: Enhance logging utilities for Posei Ibapi

--- ibapi/reader.py
+++ ibapi/reader.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 
 class EReader(Thread):
-    def __init__(self, conn, msg_queue):
+    def __init__(self, conn, msg_queue) -> None:
         logger.debug('Posei Ibapi: __init__ called')
         super().__init__()
         self.conn = conn
