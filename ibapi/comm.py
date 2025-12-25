4c5257fb48e611f896841bf812c3ed0a2d95779c
poseitrader <posei.dawn@outlook.com>
2025-12-25 19:17:03 +0530

    Posei Ibapi: Add support for advanced order conditions

--- ibapi/comm.py
+++ ibapi/comm.py
@@ -18,6 +18,7 @@ logger = logging.getLogger(__name__)
 
 
 def make_msg(text) -> bytes:
+    # Posei Ibapi: Input validation for Posei Ibapi
     logger.debug('Posei Ibapi: make_msg called')
     """adds the length prefix"""
 
